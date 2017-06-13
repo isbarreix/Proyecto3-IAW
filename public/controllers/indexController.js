eea4b9c14b8017d2406277b2475619787507511a
ISB <isbarreix@gmail.com>
2017-06-13 20:46:30 -0300

    Se agrego mapa en tab2

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -1,8 +1,34 @@
-var app = angular.module('AppMaps', ["ngMaterial"])
+var app = angular.module('AppMaps', ['ngMaterial', 'ngMap']);
 
-
-app.controller("TabOne", function($scope) {
+app.controller("TabOneCtrl", function($scope) {
 	$scope.title = "Count Upwards";
 
-
 });
+
+//	Documentacion api
+
+app.controller('MapCtrl', function(NgMap) {
+  NgMap.getMap().then(function(map) {
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
+});
+
+/*
+
+app.controller('MapCtrl', function($scope) {
+      
+       $scope.initialize = function() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+             center: {lat: -34.397, lng: 150.644},
+             zoom: 8
+          });
+       }    
+       
+       google.maps.event.addDomListener(window, 'load', $scope.initialize);   
+
+    });
+
+*/
+
