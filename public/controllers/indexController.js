7b934ca03b75655110e27f6a62c69b94cc6b5ff1
ISB <isbarreix@gmail.com>
2017-06-14 00:58:57 -0300

    Ejemplos basico de mapa

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -7,13 +7,38 @@ app.controller("TabOneCtrl", function($scope) {
 
 //	Documentacion api
 
-app.controller('MapCtrl', function(NgMap) {
-  NgMap.getMap().then(function(map) {
-    console.log(map.getCenter());
-    console.log('markers', map.markers);
-    console.log('shapes', map.shapes);
-  });
-});
+app.controller('MapCtrl', ['$scope', '$http', function ($scope, $http, NgMap) {
+  
+	// Marcadores de prueba
+	mk1 = {
+		name: 'ATM Provincia',
+		alt: '10',
+		lat: '20',
+		descrip: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+	};
+	mk2 = {
+		name: 'ATM Link',
+		alt: '30',
+		lat: '40',
+		descrip: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+	};
+	mk3 = {
+		name: 'ATM Galicia',
+		alt: '50',
+		lat: '60',
+		descrip: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.'
+	};
+
+	var markerlist = [mk1, mk2, mk3];
+	$scope.markerlist = markerlist;
+	// Iniciar Mapa 
+/*	NgMap.getMap().then(function(map) {
+		console.log(map.getCenter());
+		console.log('markers', map.markers);
+		console.log('shapes', map.shapes);
+	});*/
+
+}]);
 
 /*
 
