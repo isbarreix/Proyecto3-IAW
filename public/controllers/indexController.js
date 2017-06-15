46de3ecef7f47a64b6ca0d223c39788bec643ac5
ISB <isbarreix@gmail.com>
2017-06-15 18:03:32 -0300

    logic admin (en proceso)

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -84,3 +84,76 @@ app.controller('MapCtrl', ['$scope', '$http', function ($scope, $http, NgMap) {
     };
 
 }]);
+
+
+app.controller('AdminCtrl', ['$scope', '$http', function ($scope, $http) {
+var vm = this;
+	var map;
+	var infowindow;
+    $scope.initialize = function() {
+		var bahia = {lat: -38.7167, lng: -62.2833};
+
+        map = new google.maps.Map(document.getElementById('mapAdmin'), {
+            center: bahia,
+            zoom: 15
+        });
+        
+        google.maps.event.addListener(map, "click", function (event) {
+                var latitude = event.latLng.lat();
+                var longitude = event.latLng.lng();
+                console.log( latitude + ', ' + longitude );
+                var locacionActual = {lat: latitude, lng: longitude};
+            
+                var service = new google.maps.places.PlacesService(map);
+                service.nearbySearch({
+                    location: locacionActual,
+                      radius: 5000,
+                      type: ['atm']
+                    }, callback);
+                
+            }); //end addListener
+
+        getMarcadores();
+		
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+            location: bahia,
+              radius: 5000,
+              type: ['atm']
+            }, callback);
+        
+  
+    }        
+    google.maps.event.addDomListener(window, 'load', $scope.initialize); 
+  
+	function getMarcadores(){
+		$http.get('/markerApplist').then(function(response) {
+			//	console.log(response.data);
+			$scope.markerApplist = response.data;
+		 });
+	}
+	
+	function callback(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				createMarker(results[i]);
+			}
+			$scope.markerGooglelist = results;
+		}
+	}
+
+	function createMarker(place) {
+		var placeLoc = place.geometry.location;
+		var marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location
+		});
+
+	google.maps.event.addListener(marker, 'click', function() {
+			infowindow.setContent(place.name);
+			infowindow.open(map, this);
+		});
+	}
+
+}]);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 var express = require("express");
 var mongojs = require('mongojs');
 var db = mongojs('comments', ['comments']);
+var dbMarkerApp = mongojs('markerlist', ['markerlist']);
 var bodyParser = require("body-parser");
 var app = express();
 // carpeta node_modules
@@ -12,16 +13,13 @@ app.use('/modules', express.static(__dirname + '/node_modules/'));
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
-/*var mongojs = require('mongojs');
-//       1er arg: base de dats, 2do ar:
-//var db = mongojs('contactlist', ['contactlist']);
-;*/
+
 
 app.get("/comments", function (req, resp){
     console.log("I received a get");
     
     db.comments.find(function(err,docs){
-        console.log(docs);
+        //console.log(docs);
         resp.json(docs);
     })
 });
@@ -32,6 +30,14 @@ app.post("/comments", function(req,resp){
         resp.json(doc);
     })
 });
+
+
+app.get("/markerapplist", function(req, res) {
+    dbMarkerApp.markerlist.find(function (err, docs) {
+        res.json(docs);
+    });
+});
+
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
